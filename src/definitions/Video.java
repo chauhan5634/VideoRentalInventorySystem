eb1b37ef2e0b0e26da00560aafe5572f45c1a9ed
Nirbhay Chauhan <nirbhay.chauhan_cs19@gla.ac.in>
2020-08-30 22:42:17 +0530

    add fields for Video.java

--- src/definitions/Video.java
+++ src/definitions/Video.java
@@ -1,4 +1,17 @@
 package definitions;
 
 public class Video {
+    // member variables =?
+    // fields/variables of a class
+
+    // creating definition classes in Java
+
+    //rules for creating fields
+    // 1. Set the Visibility modifier of the fields to private unless otherwise specified.
+    // 2. Set the access modifier of the fields to non-static unless otherwise specified.
+
+
+    private String videoName;
+    private boolean checkOut;
+    private int rating;
 }
